5b6fe026024ce1fe8fd1f1688909443315ec2300
Nick Christensen <nick.g.christensen@gmail.com>
2025-10-04 14:44:18 -0500

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Nick Christensen on 2025-10-04.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
